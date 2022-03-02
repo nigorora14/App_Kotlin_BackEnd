e12121f577238f24c0923376cefb2e358401bf94
Nilton Gonzano <niltongr@outlook.com>
2022-03-01 20:30:50 -0500

    .

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
-const express = require('express') //necesario para crear una conexion
-const app = express()//necesario para crear una conexion
-const http = require('http')//necesario para crear una conexion
-const server = http.createServer(app)//necesario para crear una conexion
+const express = require('express')                              //necesario para crear una conexion
+const app = express()                                           //necesario para crear una conexion
+const http = require('http')                                    //necesario para crear una conexion
+const server = http.createServer(app)                           //necesario para crear una conexion
 
 const logger = require('morgan')
 const cors = require('cors')
 
-const port = process.env.PORT || 3000//necesario para crear una conexion
-app.set('port',port)//necesario para crear una conexion
+const port = process.env.PORT || 3000                           //necesario para crear una conexion
+app.use(logger('dev'))                      //para debugear los posibles errores
+app.use(express.json())                   //para parsear la respuesta que resivamos en formato json
+app.use(express.urlencoded({              //para parsear la respuesta que resivamos en formato json
+    extended: true                        //para parsear la respuesta que resivamos en formato json
+}))                                       //para parsear la respuesta que resivamos en formato json
+app.use(cors())
+app.disable('x-powered-by')         //para la seguridad
 
-server.listen(3000,'192.168.3.105' || 'localhost', function(){//necesario para crear una conexion
-    console.log('backend iniciando con el process :'+ process.pid+' y con el port '+port)//necesario para crear una conexion
-})//necesario para crear una conexion
+app.set('port',port)                                            //necesario para crear una conexion
+
+server.listen(3000,'192.168.3.105' || 'localhost', function(){  //necesario para crear una conexion
+    console.log('backend iniciando con el process :'+ process.pid+' y con el port '+ port)   //necesario para crear una conexion
+})                                                              //necesario para crear una conexion
+
+app.get('/', (req, res) => { 
+    res.send('Ruta raiz del backend')
+})
+
+//Error handler
+app.use((err, req, res, next) => {
+    console.log(err)
+    res.status(err.status || 500).send(err.stack)
+})
